001ccc6b4e135896a66ede70a1717288e225e5d2
Zaki003 <zakifaiazchy.work@gmail.com>
2021-09-26 23:09:43 +0600

    all modules practiced

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
 
+
   return (
     <div className="App">
-      <Person name="Zaki" job="Student" age='23'></Person>
-      <Person name="Drizzle" job="Teacher" age='21'></Person>
+      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
+    </div>
+  );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  return (
+    <div>
+      <h3>External Users</h3>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div className="person">
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   );
 }
@@ -20,6 +65,28 @@ function Person(props) {
       <h4>Proffession: {props.job}</h4>
     </div>
   );
-}
+};
+
+function Cinema(props) {
+  return (
+    <div className="person">
+      <h1>Nayok: {props.nayok}</h1>
+      <h2>Nayika: {props.nayika}</h2>
+    </div>
+  );
+};
 
 export default App;
+
+/* const actors = [
+  { nayok: "Jashim", nayika: "Sabrina" },
+  { nayok: "Alomgir", nayika: "Shabana" },
+  { nayok: "Razzak", nayika: "Shuchona" },
+  { nayok: "Riaz", nayika: "Shabnur" }
+]; */
+
+{/* <Person name="Zaki" job="Student" age='23'></Person>
+      <Person name="Drizzle" job="Teacher" age='21'></Person> */}
+     /*  {
+actors.map(actor => <Cinema nayok={actor.nayok} nayika={actor.nayika}></Cinema>)
+}  */
